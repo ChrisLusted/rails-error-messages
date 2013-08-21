18c97d91e3114edc216c1ac2517b23d1183e61c2
owain lewis <owain@owainlewis.com>
2013-08-21 19:32:59 +0100

    adds shell script

---
+++ install.sh
@@ -0,0 +1,12 @@
+read -p "Which themes would you like to install? (light|dark) " RESP
+if [ "$RESP" = "dark" ]; then
+  echo "Installing dark themes"
+  curl -O https://raw.github.com/owainlewis/rails-error-messages/master/dark/404.html
+  curl -O https://raw.github.com/owainlewis/rails-error-messages/master/dark/422.html
+  curl -O https://raw.github.com/owainlewis/rails-error-messages/master/dark/500.html
+else
+  echo "Installing light themes"
+	curl -O https://raw.github.com/owainlewis/rails-error-messages/master/light/404.html
+  curl -O https://raw.github.com/owainlewis/rails-error-messages/master/light/422.html
+  curl -O https://raw.github.com/owainlewis/rails-error-messages/master/light/500.html
+fi
